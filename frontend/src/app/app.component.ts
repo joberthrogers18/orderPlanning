ee8fd7889b17d9e06c10ec36a0cae202dfaa1375
Joberth Rogers <joberth.rogers18@gmail.com>
2025-02-19 01:39:36 -0300

    feat: add navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule],
+  imports: [RouterModule, MenubarModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'Planning Order';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Pedidos',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Fornecedores',
+        icon: 'pi pi-fw pi-truck',
+      },
+      {
+        label: 'Clientes',
+        icon: 'pi pi-fw pi-users',
+      },
+      {
+        label: 'Produtos',
+        icon: 'pi pi-fw pi-cart-minus',
+      },
+      
+    ]
+  }
+
 }
