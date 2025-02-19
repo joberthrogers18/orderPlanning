681b16a2a9b311bf7e662b798b863f88d4c8d5a1
Joberth Rogers <joberth.rogers18@gmail.com>
2025-02-18 21:22:59 -0300

    chore: init my backend

---
+++ order/src/main/java/com/demo/orderPlanning/order/OrderPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.demo.orderPlanning.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPlanningApplication.class, args);
+	}
+
+}
---
+++ order/src/test/java/com/demo/orderPlanning/order/OrderPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.orderPlanning.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
