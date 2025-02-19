8c0c28460dda372205503c52b2a5b0ea174233f1
Joberth Rogers <joberth.rogers18@gmail.com>
2025-02-18 23:38:35 -0300

    fix: add person class abstract to buyer and supplier

--- order/src/main/java/com/demo/orderPlanning/order/entity/Buyer.java
+++ order/src/main/java/com/demo/orderPlanning/order/entity/Buyer.java
@@ -1,21 +1,10 @@
 package com.demo.orderPlanning.order.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Buyer {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
-}
+public class Buyer extends Person { }
---
+++ order/src/main/java/com/demo/orderPlanning/order/entity/Person.java
@@ -0,0 +1,16 @@
+package com.demo.orderPlanning.order.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
